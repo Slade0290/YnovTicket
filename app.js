19410d6839c1182a9b26c9dff8103c7f66b37d74
Tanguy Ralaimiadana <tanguy.rdn@gmail.com>
2019-03-07 17:47:23 +0100

    add node app core

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.setHeader('Content-type','application/json');
+    res.setHeader('Accept','application/json');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin');
+    next();
+});
+
+//connecting to the db
+mongoose.connect('mongodb://pota:b3cticketsup@ds361085.mlab.com:61085/ticketsup', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database');
+    process.exit();
+});
+
+
+//listening to request
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
